0e199cbe6ccef3387fe54b9b23772f7899a5af2d
Sumaiya-Ansari-Github <162428320+Sumaiya-Ansari-Github@users.noreply.github.com>
2024-03-06 00:56:20 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+let per = "40"
+if(per==='90<100'){
+    console.log("you got A+ grade");
+}
+else if(per==='80<90'){
+    console.log("you got A grade");
+}
+else if(per==='70<80'){
+    console.log("you got B grade");
+}
+else if(per==='60<70'){
+    console.log("you got C grade");
+}
+else if(per==='60<50'){
+    console.log("you got D grade");
+}
+else{
+    console.log("You are fail");
+}
